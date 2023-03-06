0585dd3e5aca49bb91199b3f61bc7be5f3791f78
Siyuan You <satellitem@me.com>
2023-03-06 19:22:27 +1100

    node adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
-  integrations: [preact()]
+	output: "server",
+	adapter: node({
+		mode: "standalone"
+	}),
+	integrations: [preact()]
 });
