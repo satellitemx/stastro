a12f92ece5dd8820320ce392abaa8915e6ec94eb
Siyuan You <satellitem@me.com>
2023-06-27 21:51:55 +1000

    migrate nost db from supabase to firebase

--- src/pages/api/nost/[noteId].ts
+++ src/pages/api/nost/[noteId].ts
@@ -1,23 +1,24 @@
-import type { APIContext } from "astro";
-import hashids from "src/lib/hashids";
-import supabase from "src/lib/supabase";
+import type { APIRoute } from "astro";
+import { db } from "src/firebase/server";
 
-export const post = async (ctx: APIContext) => {
-	if (ctx.request.method !== "POST") {
-		return new Response("Invalid HTTP Method.", { status: 400 })
-	}
+export const post: APIRoute = async (ctx) => {
 	const { noteId } = ctx.params
 	if (!noteId) {
-		return new Response("No Nost address supplied.", { status: 400 })
-	}
-	const id = hashids.decode(noteId).pop()
-	if (id === undefined) {
-		return new Response("Cannot decode Nost address.", { status: 404 })
+		return new Response("No Nost address.", { status: 400 })
 	}
 	const payload = await ctx.request.formData()
-	const { error, statusText } = await supabase.from("nost").upsert({ id: id, content: payload.get("content") })
-	if (error) {
-		return new Response(error.message, { status: 500 })
+	const content = payload.get("content")
+
+	try {
+		await db.collection("nost").doc(noteId).set({
+			content,
+		}, {
+			mergeFields: ["content"]
+		})
+	} catch (e) {
+		console.log(e)
+		return new Response("Failed to update document", { status: 500 })
 	}
-	return new Response(statusText)
+
+	return new Response("success")
 }
