26b87744a232589590a0bea7645b0146a810e16b
Siyuan You <satellitem@me.com>
2023-07-13 16:26:27 +1000

    styling with tailwind utility classes

--- astro.config.ts
+++ astro.config.ts
@@ -1,29 +1,27 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
 // import vercel from "@astrojs/vercel/edge";
 
-// https://astro.build/config
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-	output: "server",
-	adapter: vercel(),
-	integrations: [
-		preact({
-			compat: true
-		}),
-		svelte({
-			compilerOptions: {
-				customElement: true,
-			}
-		})
-	],
-	build: {
-		format: "directory"
-	}
+  output: "server",
+  adapter: vercel(),
+  integrations: [preact({
+    compat: true
+  }), svelte({
+    compilerOptions: {
+      customElement: true
+    }
+  }), tailwind()],
+  build: {
+    format: "directory"
+  }
 });
